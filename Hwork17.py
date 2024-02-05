9fc3e2c02e0491899733e33ee92d07dc6fc8c354
Ilia Chutlashvili <i.chutlashvili69@gmail.com>
2024-02-05 10:11:38 +0400

    Hwork 17 update

---
+++ Hwork17.py
@@ -0,0 +1,31 @@
+# # დავალება 1 
+# class SampleClass:
+#     def __init__(self, public, protected, private):
+#         self._public = public 
+#         self.__private = private  
+
+#     @property
+#     def public(self):
+#         return self._public
+
+#     @public.setter
+#     def public(self, value):
+#         self._public = value
+
+#     @property
+#     def protected(self):
+#         return self._protected_param
+
+#     @protected.setter
+#     def protected(self, value):
+#         self._protected_param = value
+
+#     @property
+#     def private(self):
+#         return self.__private
+
+#     @private.setter
+#     def private(self, value):
+#         self.__private = value
+
+# sample_instance.protected = 20
