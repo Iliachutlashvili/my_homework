ad562e7687041786061c14972087a0ea5e4264b5
Ilia Chutlashvili <i.chutlashvili69@gmail.com>
2024-02-16 17:24:58 +0400

    Code fix, + Comments

--- project.py
+++ project.py
@@ -14,30 +14,33 @@ def guess_the_number():
     
     print("Welcome to Guess the Number","\nChoose number from 1 to 20?")
     
+    # ეს ციკლი გვჭირდება იმისთვის რომ სადამ მაქსიმალური მცდელობა მეტი იქნება 0 ზე, გამოვიყენოთ ჩვენი ცდის რაოდენობა.
     while attempts < max_attempts:
-        try:
-            user_input = input("Enter your guess (between 1 and 100), or type 'exit' to end the game: ")
-            if user_input.lower() == 'exit':  #თუ თამაში მოგბეზრდა და დამთავრება გსურს exit ქომენდით დავასრულებთ თამაშს
-                return
-            guess = int(input("Enter your guess (between 1 and 20): "))
-            if guess < 1 or guess > 20:
-                print("I Said from 1 to 20! .")
+            try:
+                #თუ თამაშის დამთავრება გსურს exit ქომენდით დავასრულებთ თამაშს
+                user_input = input("Enter your guess (between 1 and 20), or type 'exit' to end the game: ")
+                if user_input.lower() == 'exit':
+                    return
+                guess = int(user_input)
+                if guess < 1 or guess > 20:
+                    print("Please enter a number between 1 and 20.")
+                    continue
+            except ValueError:
+                print("Please enter a valid number or type 'exit' to end the game.")
                 continue
-        except ValueError:
-            print("Please enter a valid number.")
-            continue
-        
-        attempts += 1
-        
-        if guess < secret_number:
-            print("Too low! Try again.")
-        elif guess > secret_number:
-            print("Too high! Try again.")
-        else:
-            print(f"Congratulations! You've guessed the number {secret_number} correctly in {attempts} attempts!")
-            break
+            # თუ ისეთი ციფრი შეიყვანა რაც მოცემული არაა 1 დან 20 ის ჩათვლით ის 1 ცდა უკან გიბრუნდება.
+            attempts += 1   
+            
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You've guessed the number {secret_number} correctly in {attempts} attempts!")
+                break
     else:
         print(f"Sorry, you've run out of attempts. The number was {secret_number}.")
-
+    
 if __name__ == "__main__":
     guess_the_number()
+
